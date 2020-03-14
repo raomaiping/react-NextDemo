f97b2f918a82ce001b85761693220a3e00d3c69c
2582395486@qq.com <2582395486@qq.com>
2020-03-14 16:15:05 +0800

    路由的六个钩子事件

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,31 @@ import React from 'react'
 import Link from 'next/link'
 import Router from 'next/router'
 const Home=()=>{
+
+  //routeChangeStart
+  //routeChangeComplete
+  //beforeHistoryChange
+  //routeChangeError
+  //hashChangeStart
+  //hashChangeComplete
+  Router.events.on('routeChangeStart',(...arsg)=>{
+    console.log('1.routeChangeStart->路由开始变化，参数为:',...arsg);
+  })
+  Router.events.on('routeChangeComplete',(...arsg)=>{
+    console.log('2.routeChangeComplete->路由变化结束，参数为:',...arsg);
+  })
+  Router.events.on('beforeHistoryChange',(...arsg)=>{
+    console.log('3.beforeHistoryChange->路由变化结束，参数为:',...arsg);
+  })
+  Router.events.on('routeChangeError',(...arsg)=>{
+    console.log('4.routeChangeError->路由发生错误，参数为:',...arsg);
+  })
+  Router.events.on('hashChangeStart',(...arsg)=>{
+    console.log('5.hashChangeStart->，参数为:',...arsg);
+  })
+  Router.events.on('hashChangeComplete',(...arsg)=>{
+    console.log('6.hashChangeComplete->，参数为:',...arsg);
+  })
   function gotoXiaojiejie(){
     // Router.push('/xiaojiejie?name=苍井空')
     Router.push({
@@ -19,6 +44,9 @@ const Home=()=>{
         <div><Link href="/xiaojiejie?name=苍井空"><a>苍井空</a></Link></div>
         <div>
             <button onClick={gotoXiaojiejie}>去找苍井空</button>
+        </div>
+        <div>
+            <Link href="#rmp"><a>选前端小菜鸟吖</a></Link>
         </div>
       </>
   )

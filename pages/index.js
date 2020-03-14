4901314abc25aa2d6e79e4e5fd76a5ad4d63a08f
2582395486@qq.com <2582395486@qq.com>
2020-03-14 15:42:16 +0800

    使用query 来传递参数和接收参数

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,18 @@ import React from 'react'
 import Link from 'next/link'
 import Router from 'next/router'
 const Home=()=>{
-  function gotoA(){
-    Router.push('/rmpA')
+  function gotoXiaojiejie(){
+    Router.push('/xiaojiejie?name=苍井空')
   }
   return(
       <>
         <div>我是首页</div>
         <div><Link href="/rmpA"><a>去rmp A页面</a></Link></div>
         <div><Link href="/rmpB"><a>去rmp B页面</a></Link></div>
+        <div><Link href="/xiaojiejie?name=玛利亚"><a>玛利亚</a></Link></div>
+        <div><Link href="/xiaojiejie?name=苍井空"><a>苍井空</a></Link></div>
         <div>
-            <button onClick={gotoA}>去rmp--A</button>
+            <button onClick={gotoXiaojiejie}>去找苍井空</button>
         </div>
       </>
   )
